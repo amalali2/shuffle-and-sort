8f09eccebd562a4ac38d62a0598d364ebaed834e
Amal <435amal@gmail.com>
2022-08-31 13:47:59 +0100

    update script.js
    inject number list and add identifiers

---
+++ script.js
@@ -0,0 +1,6 @@
+let numbersList = ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  numbersWrapper = document.getElementById('container'), appendCubeElement;
+
+numbersList.forEach((item, index) => {
+  numbersWrapper.innerHTML += '<li data-item=' + item + '>' + item + '</li>'
+})
