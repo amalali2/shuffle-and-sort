a8902a149f3686a1d0c238b9c286691ebb60513d
Amal <435amal@gmail.com>
2022-08-31 13:50:52 +0100

    add shuffle and sort function names

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let numbersList = ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
 numbersList.forEach((item, index) => {
   numbersWrapper.innerHTML += '<li data-item=' + item + '>' + item + '</li>'
 })
+
+shuffleList = () => {
+
+}
+
+sortList = () => {
+
+}
